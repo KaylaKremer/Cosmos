3e9ce777706c3b59dc6ec393a7cc3257ab6ea7fa
Kayla Kremer <kremer.kayla@gmail.com>
2019-11-17 20:28:26 -0500

    Create initial project files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Viget from './Viget';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Viget />
+    </div>
+  );
+}
+
+export default App;
