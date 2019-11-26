227c624d95e4cef1fcf343cca23ee421c4d209b5
Kayla Kremer <kremer.kayla@gmail.com>
2019-11-26 00:19:54 -0500

    Refactor class names, update README

--- README.md
+++ README.md
@@ -1,16 +1,5 @@
 # Hello, Viget! 👋
 
-My name is Kayla and I'm a front-end developer in the NoVA area. Nice to meet you! 😄
-
-I built this small app with React and THREE.js using your logo as inspiration. I've only recently started learning THREE.js on my own, so please forgive the simplicity of it. 😅
-
-If you haven't already been able to tell, I love anything 3D! Actually, my bachelors from George Mason University was in videogame design, so I spent a lot of my university days learning how to 3D model with programs like 3ds Max and Unity.
-
-For the past 2.5 years, I've worked as a front-end developer professionally. I'm a self-taught developer, no bootcamp here! Just a determination of wanting to know more than just design, which quickly turned into a passion and then a career.
-
-JavaScript is my preferred language to code in and I love learning new libraries/frameworks for it. Recently, another library I've gotten into is Google's machine learning library for the web called TensorFlow.js, which I also built a small app with. It even got me noticed by O'Reilly Media and I was invited to attend the TensorFlow World conference last month!
-
-...WIP
 
 
 
--- src/js/Menu.js
+++ src/js/Menu.js
@@ -10,9 +10,9 @@ const Menu = props => {
         <div className="menu-container">
           <input type="checkbox" id="menu-toggler" className="input-checkbox" defaultChecked={props.menu} onChange={props.toggleMenu} />
           <label htmlFor="menu-toggler" id="menuTogglerLabel" className="menu-toggler">
-            <span className="menu-toggler__line"></span>
-            <span className="menu-toggler__line"></span>
-            <span className="menu-toggler__line"></span>
+            <span className="menu-toggler-line"></span>
+            <span className="menu-toggler-line"></span>
+            <span className="menu-toggler-line"></span>
           </label>
           <nav id="sidebar" className="sidebar">
             <div className="image-container">
@@ -21,11 +21,11 @@ const Menu = props => {
             </div>
             <div className="menubar-container">
                 <ul id="menubar" className="menu">
-                    <li className="menu__item"><a className="menu__link" target="_blank" rel="noopener noreferrer" href="https://github.com/KaylaKremer"><FontAwesomeIcon icon={faGithub} /> GitHub</a></li>
-                    <li className="menu__item"><a className="menu__link" target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/kaylakremer/"><FontAwesomeIcon icon={faLinkedin} /> LinkedIn</a></li>
-                    <li className="menu__item"><a className="menu__link" target="_blank" rel="noopener noreferrer" href="https://www.kaylakremer.com/"><FontAwesomeIcon icon={faPalette} /> Portfolio</a></li>
-                    <li className="menu__item"><a className="menu__link" target="_blank" rel="noopener noreferrer" href="https://twitter.com/home"><FontAwesomeIcon icon={faTwitter} /> Twitter</a></li>
-                    <li className="menu__item"><a className="menu__link" href="mailto:kremer.kayla@gmail.com"><FontAwesomeIcon icon={faEnvelope} /> Contact</a></li>
+                    <li className="menu-item"><a className="menu-link" target="_blank" rel="noopener noreferrer" href="https://github.com/KaylaKremer"><FontAwesomeIcon icon={faGithub} /> GitHub</a></li>
+                    <li className="menu-item"><a className="menu-link" target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/kaylakremer/"><FontAwesomeIcon icon={faLinkedin} /> LinkedIn</a></li>
+                    <li className="menu-item"><a className="menu-link" target="_blank" rel="noopener noreferrer" href="https://www.kaylakremer.com/"><FontAwesomeIcon icon={faPalette} /> Portfolio</a></li>
+                    <li className="menu-item"><a className="menu-link" target="_blank" rel="noopener noreferrer" href="https://twitter.com/home"><FontAwesomeIcon icon={faTwitter} /> Twitter</a></li>
+                    <li className="menu-item"><a className="menu-link" href="mailto:kremer.kayla@gmail.com"><FontAwesomeIcon icon={faEnvelope} /> Contact</a></li>
                 </ul>
             </div>
           </nav>
