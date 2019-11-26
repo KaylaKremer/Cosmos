9262a7ec36a9e82a953fe88429b11d431826fcb6
Kayla Kremer <kremer.kayla@gmail.com>
2019-11-25 21:27:33 -0500

    Make improvements to menu scss

--- src/js/Menu.js
+++ src/js/Menu.js
@@ -18,11 +18,11 @@ export default class Menu extends Component {
               <nav id="sidebar" className="sidebar">
                 <div>Check out Kayla's Stuff!</div>
                 <ul id="menubar" className="menu">
-                  <li className="menu__item"><a className="menu__link" href="#">Home</a></li>
-                  <li className="menu__item"><a className="menu__link" href="#">Blog</a></li>
-                  <li className="menu__item"><a className="menu__link" href="#">Portfolio</a></li>
-                  <li className="menu__item"><a className="menu__link" href="#">About</a></li>
-                  <li className="menu__item"><a className="menu__link" href="#">Contact</a></li>
+                  <li className="menu__item"><a className="menu__link" target="_blank" rel="noopener noreferrer" href="https://github.com/KaylaKremer">GitHub</a></li>
+                  <li className="menu__item"><a className="menu__link" target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/kaylakremer/">LinkedIn</a></li>
+                  <li className="menu__item"><a className="menu__link" target="_blank" rel="noopener noreferrer" href="https://www.kaylakremer.com/">Portfolio</a></li>
+                  <li className="menu__item"><a className="menu__link" target="_blank" rel="noopener noreferrer" href="https://twitter.com/home">Twitter</a></li>
+                  <li className="menu__item"><a className="menu__link" href="mailto:kremer.kayla@gmail.com">Contact</a></li>
                 </ul>
               </nav>
             </div>
